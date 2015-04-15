6400e8dd89eae5678da3997c6b0575b61efe8377
youxanjump <maydayisgod@hotmail.com.tw>
2015-04-16 01:46:02 +0800

    ArrayList類別已繼承
    把ArrayList下來後
    要使用其method用super()語法呼叫它的建構函式
    就可以使用了(保險的作法，個人習慣)
    
    因為ArrayList一開始的型態是Object
    所以要將其強制轉成String就可以使用了
    ex: 15 19行

--- testMyStack.java
+++ testMyStack.java
@@ -1,32 +1,28 @@
-# U10316033_HW_11_10
-修改範例程式11.10，利用現有方法及繼承ArrayList，倒著print出字串
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class myStack{
-	private ArrayList<String> list = new ArrayList<>();
+class myStack extends ArrayList{
 	
 	public boolean isEmpty(){
-		return list.isEmpty();
+		return super.isEmpty();
 	}
 	
 	public int getSize(){
-		return list.size();
+		return super.size();
 	}
 	
 	public String peek(){
-		return list.get(getSize() - 1);
+		return (String)super.get(getSize() - 1);
 	}
 	
 	public String pop(){
-		String o = list.get(getSize() - 1);
-		list.remove(getSize() - 1);
+		String o = (String)super.get(getSize() - 1);
+		super.remove(this.getSize() - 1);
 		return o;
 	}
 	
 	public void push(String o){
-		list.add(o);
+		super.add(o);
 	}
 	
 	public void printString(){
@@ -37,17 +33,17 @@ class myStack{
 	
 		for(int x = 0 ; x < 5 ; x++){
 			a[x] = input.next();
-			this.push(a[x]);
+			push(a[x]);
 		}
 
 		for(int x = 1 ; x <= 5 ; x++){
-			System.out.print(this.pop() + " ");
+			System.out.print(pop() + " ");
 		}
 	}
 	
 	@Override
 	public String toString(){
-		return "Stack: " + list.toString();
+		return "Stack: " + super.toString();
 	}
 }
 
